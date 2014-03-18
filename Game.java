e2f63c8700ff06b86214b25afff00b9b0be20d71
apcs <apcs@mvhs-u528ap02.dcsdk12.org>
2014-03-18 08:53:33 -0600

    Started

--- Game.java
+++ Game.java
@@ -1,5 +1,34 @@
 public class Game {
 
 
+	public static void main(String[] args) {
+		//win(team1,team2,wp1,wp2);
+		
+	
+}
+	public Game(String[] values) {
+		for (int i=0;i<values.length;i++) {
+			 String team1 = values[i];
+			 String team2 = values[i+3];
+			 double wp1 = Double.parseDouble(values[i+2]);
+			 double wp2 = Double.parseDouble(values[i+5]);
+
+			//System.out.println(win(team1,team2,wp1,wp2));
+			 System.out.println(team1);
+
+			 win(team1,team2,wp1,wp2);
+		}
+
+
+	}
+
+
+	public void win(String team1, String team2, double wp1, double wp2) {
+		double top = (wp1 - (wp1 * wp2));
+		double bottom = wp1 + wp2 - (2*wp1 * wp2);
+		double win = top / bottom;
+
+		System.out.println(win);
+	}
 
 }
--- ReadNCAA.java
+++ ReadNCAA.java
@@ -1,7 +1,10 @@
 import java.util.Scanner;
 import java.io.*;
+import java.util.ArrayList;
 
 public class ReadNCAA {
+
+
 
 	public static void main(String[] args)  throws FileNotFoundException {
 		Scanner reader = new Scanner(new File("ncaa2014.csv"));
@@ -10,10 +13,39 @@ public class ReadNCAA {
 			String next = reader.nextLine();
 			String[] values = next.split(",");
 
-			for (String s : values) {
-				System.out.print(s + " ");
-			}
+			//for (String s : values) {
+				//System.out.print(s + " ");
+			//}
+			
+			ArrayList<String> names = new ArrayList<String>();
+			ArrayList<Integer> seeds = new ArrayList<Integer>();
+			ArrayList<Double> winp = new ArrayList<Double>();
 
+
+			for (int i=0, j=0; i<values.length; i++, j++) {
+				if(j==0) {
+					String name = values[i];
+					System.out.println(name + " ");
+					names.add(name);
+					
+				} else if(j==1) {
+					int seed = Integer.parseInt(values[i]);
+					System.out.println(seed);
+					seeds.add(seed);
+					
+				} else if(j==2) {
+					double wp = Double.parseDouble(values[i]);
+					System.out.println(wp);
+					winp.add(wp);
+				}
+				
+				j=1;
+				
+				
+			}
+			//System.out.print(names);
+			//System.out.print(seeds);
+			//System.out.print(winp);
 			System.out.println();
 		}
 	}
